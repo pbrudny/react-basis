cf4ee1b541f940f694a4e816edd1a247eef06717
pbrudny <pbrudny@gmail.com>
2017-09-23 12:55:45 +0200

    Fix styling

--- react_2_5_course/components/Contact.js
+++ react_2_5_course/components/Contact.js
@@ -9,10 +9,12 @@ var Contact = React.createClass({
                     className: 'contactImage',
                     src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
                 }),
-                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactLabel', href: 'mailto:' + this.props.item.email},
-                    this.props.item.email
+                React.createElement('div', {className: 'details'},
+                    React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+                    React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+                    React.createElement('a', {className: 'contactLabel', href: 'mailto:' + this.props.item.email},
+                        this.props.item.email
+                    )
                 )
             )
         )
