dfa268a3efe4c5154b7daad782f4d4f2f8c9ad70
pbrudny <pbrudny@gmail.com>
2017-09-23 12:51:49 +0200

    Move to JSX

--- react_2_7/components/Contact.js
+++ react_2_7/components/Contact.js
@@ -1,20 +1,19 @@
 var Contact = React.createClass({
     propTypes: {
-        item: React.PropTypes.object.isRequired
+        item: React.PropTypes.object.isRequired,
     },
+
     render: function() {
         return (
-            React.createElement('div', {className: 'contactItem'},
-                React.createElement('img', {
-                    className: 'contactImage',
-                    src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
-                }),
-                React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-                React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-                React.createElement('a', {className: 'contactLabel', href: 'mailto:' + this.props.item.email},
-                    this.props.item.email
-                )
-            )
-        )
+            <div className='contactItem'>
+                <img className='contactImage' src='http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico' />
+                <div className='details'>
+                    <p className='contactLabel'>Imię:{this.props.item.firstName} </p>
+                    <p className='contactLabel'>Nazwisko:{this.props.item.lastName} </p>
+                    <a className='contactLabel' href={'mailto:' + this.props.item.email}> {this.props.item.email} </a>
+                </div>
+            </div>
+        );
     }
 });
+
---
+++ react_2_7_course/Hello.js
@@ -0,0 +1,7 @@
+var Hello = React.createClass({
+    render: function() {
+        return (
+            <div>Hello!</div>
+        )
+    }
+});
